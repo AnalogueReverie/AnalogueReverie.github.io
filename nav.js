3e47f81c5ecd26a1a109898ffe94e1a155d44202
WishMakers <35705292+WishMakers0@users.noreply.github.com>
2022-07-16 01:20:55 -0400

    vycm site

---
+++ nav.js
@@ -0,0 +1,11 @@
+document.write('<div>\
+<img src="img/arlogofull.png" alt="Analogue Reverie logo" style="width:400px; height:230px; max-width:100%; border: 0px; border-radius: 0px;">\
+<table class="navigation">\
+<tr>\
+<td>\
+<a href="index.html" class="navigation">Home</a>&nbsp;• <a href="projects.html" class="navigation">Projects</a>&nbsp;• <a href="extras.html" class="navigation">Extras</a>&nbsp;• <a href="staff.html" class="navigation">Staff</a>&nbsp;• <a href="contact.html" class="navigation">Contact</a>&nbsp;• <a href="support.html" class="navigation">Support&nbsp;Us</a>\
+</td>\
+</tr>\
+</table>\
+    </div>\
+');
